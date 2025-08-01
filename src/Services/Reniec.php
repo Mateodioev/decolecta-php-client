2ea7a110adc1fe6bb8259b7c31c3974d95bb3e64
Mateo O. <orehurcomito@gmail.com>
2025-08-01 11:13:26 -0500

    add reniec service

--- src/Services/Reniec.php
+++ src/Services/Reniec.php
@@ -6,4 +6,16 @@ use DecolectaApi\Client\Client;
 
 class Reniec extends AbstractService
 {
+    /**
+     * Extraer información de personas 
+     * @param int|string $numero
+     * @return array
+     * @see https://decolecta.gitbook.io/docs/servicios/integrations-2#dni-consulta-de-datos-personales
+     */
+    public function dni(int|string $numero): array
+    {
+        return $this->client->call('GET', '/v1/reniec/dni', [
+            'numero' => $numero,
+        ]);
+    }
 }
